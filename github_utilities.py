114a4383d8c59733c53b338c8421d617d42cf14c
Andrew Stec <andrewstec@gmail.com>
2020-01-21 12:22:35 -0800

    Removed deprecated code, added logging with repo removal counts

--- github_utilities.py
+++ github_utilities.py
@@ -47,41 +47,59 @@ def set_api_path():
 	set_org_prompt = 'What is the API path (ie. https://github-dev.students.cs.ubc.ca/api/v3)? '
 	return get_input(set_org_prompt).strip()
 
-def request(endpoint_url):
+def request(endpoint_url, verb='get'):
 	headers = get_headers(api_token)
-	response = requests.get(endpoint_url, headers=headers)
+	response = requests.request(verb, endpoint_url, headers=headers)
 	if response.status_code >=200 and response.status_code <300:
-		data = json.loads(response.content)
-		return data
+		return response
 	else: 
 		print('API ERROR: ' + str(response.status_code))
 		raise SystemError('Could not connect to API. Status code: ' + response.status_code)
-	return data
 
-def get_org_team_ids():
+def get_org_teams():
+	print('GithubUtilities:: get_org_teams() - start')
 	endpoint_url = '{0}/orgs/{1}/teams'.format(api_path, github_org)
-	teamIds = []
-	teams = request(endpoint_url)
-	for team in teams:
-		print(team)
-		teamIds.append(team['id'])
-	return teamIds
+	teams = request(endpoint_url).json()
+	print('GithubUtilities:: get_org_teams() - ' + str(len(teams)) + ' teams found in ' + github_org)
+	return teams
 
-def get_all_repos_per_team(team_ids):
-	allReposPerTeam = []
-	for team_id in team_ids: 
+def get_all_repos_per_team(teams):
+	print('GithubUtilities:: get_all_repos_per_team - start')
+	all_repos_per_team = {}
+	for team in teams: 
+		team_name = team['name']
+		team_id = team['id']
 		endpoint_url = '{0}/teams/{1}/repos'.format(api_path, team_id)
-		print(endpoint_url)
-		reposPerTeam = request(endpoint_url)
-		print(reposPerTeam)
-	# allReposPerTeam.append(reposPerTeam)
-	return allReposPerTeam
+		repos_per_team = request(endpoint_url).json()
+		all_repos_per_team[team_name] = repos_per_team
+		print('GithubUtilities:: get_all_repos_per_team() - ' + team_name + ':: ' + str(len(repos_per_team)) + ' repos found')
+	return all_repos_per_team
 
-def remove_repos_from_team(team, repos):
-	for repos in team.repos:
-		team_id = team.id
-		data = request(endpoint_url)
-		print(data)
+def remove_repos_from_team(team_id, owner, repo_name):
+	print('GithubUtilities:: remove_repos_from_team() - start')
+	# endpoint_url = '{0}/teams/{1}/repos/{2}/{3}'.format(api_path, team_id, owner, repo_name) #delete method
+	# print(endpoint_url)
+
+
+def remove_all_repos_from_teams(all_repos_per_team):
+	print('GithubUtilities:: remove_all_repos_from_teams() - start')
+	total_repos_removed = 0
+	for key in all_repos_per_team.keys():
+		team_name = key
+		repos_removed = 0
+		for repo_in_team in all_repos_per_team[key]:
+			team_id = repo_in_team['id']
+			owner = repo_in_team['owner']['login']
+			repo_name = repo_in_team['name']
+			endpoint_url_test = '{0}/teams/{1}/repos/{2}/{3}'.format(api_path, '2390', 'cpsc210-2019w-t1', 'test_repo') #delete method
+			response = request(endpoint_url_test, 'delete')
+			if response.status_code == 204: 
+				# print('GithubUtilities:: Removed ' + repo_name + ' from ' + team_name)
+				repos_removed+=1 
+		print('GithubUtilities:: Finished removing ' + str(repos_removed) + ' from team ' + team_name)
+		total_repos_removed+=repos_removed
+	print('GithubUtilities:: Total Repos Removed from Teams: ' + str(total_repos_removed))
+
 
 course = get_input('What is your course number (ie. 436v)? ')
 github_org = set_github_org()
@@ -90,23 +108,7 @@ api_path = set_api_path()
 print(api_token)
 print(github_org)
 print(api_path)
-teams = get_org_team_ids()
-allReposPerTeam = get_all_repos_per_team(teams)
-print(allReposPerTeam)
-# removeAllReposPerTeam = remove_all_repos_per_team(allReposPerTeam)
-# repos = get_organization_repos()
-
-
-# def delete_organization_repos(org_name, repoNames):
-# 	for repoName in repoNames:
-# 		endpoint_url = url_base + '/repos/{0}/{1}'.format(org_name, repoName)
-# 		print(endpoint_url)
-# 		response = requests.delete(endpoint_url, headers=headers)
-# 		if response.status_code >=200 and response.status_code <300:
-# 			print('successfully deleted ' + repoName)
-# 		else:
-# 			print('could not delete ' + repoName + ' - status code: ' + str(response.status_code))
-
-# repoNames = get_organization_repos(ORG_NAME)
-# delete_organization_repos(ORG_NAME, repoNames)
-# print('done deleting repos')
+teams = get_org_teams()
+all_repos_per_team = get_all_repos_per_team(teams)
+remove_all_repos_from_teams(all_repos_per_team)
+# print(all_repos_per_team)
