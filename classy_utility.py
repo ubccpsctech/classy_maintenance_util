3b3bdc757b204e6d5542e0ee4818772b987fffd4
Andrew Stec <andrewstec@gmail.com>
2020-01-22 13:17:56 -0800

    Removed buggy code

--- classy_utility.py
+++ classy_utility.py
@@ -136,34 +136,15 @@ def remove_all_repos_from_teams(teams, all_repos_per_team):
 		if team_name in all_repos_per_team:
 			repos_removed = 0
 			for repo_in_team in all_repos_per_team[team_name]:
-				owner = repo_in_team['owner']['login']
-				repo_name = repo_in_team['name']
-				endpoint_url_test = '{0}/teams/{1}/repos/{2}/{3}'.format(api_path, team_id, owner, repo_name) #delete method
+				endpoint_url_test = '{0}/teams/{1}/repos/{2}/{3}'.format(api_path, team_id, repo_in_team['owner']['login'], repo_in_team['name']) #delete method
 				print('DEBUG ' + endpoint_url_test)
 				response = request(endpoint_url_test, 'delete')
 				if response.status_code == 204: 
-					print('GithubUtilities:: Removed ' + repo_name + ' from ' + team_name)
+					print('GithubUtilities:: Removed ' + repo_in_team['name'] + ' from ' + team_name)
 					repos_removed+=1 
-				print('GithubUtilities:: Finished removing ' + str(repos_removed) + ' repo (' + repo_name + ') from team ' + team_name)
+			print('GithubUtilities:: Finished removing ' + str(repos_removed) + ' repo (' + repo_in_team['name'] + ') from team ' + team_name)
 			num_repos_removed+=repos_removed
 	return num_repos_removed
-
-	# for key in all_repos_per_team.keys():
-	# 	team_name = key
-	# 	repos_removed = 0
-	# 	for repo_in_team in all_repos_per_team[key]:
-	# 		team_id = repo_in_team['id']
-	# 		owner = repo_in_team['owner']['login']
-	# 		repo_name = repo_in_team['name']
-	# 		endpoint_url_test = '{0}/teams/{1}/repos/{2}/{3}'.format(api_path, team_id, owner, repo_name) #delete method
-	# 		print('DEBUG ' + endpoint_url_test)
-	# 		response = request(endpoint_url_test, 'delete')
-	# 		if response.status_code == 204: 
-	# 			print('GithubUtilities:: Removed ' + repo_name + ' from ' + team_name)
-	# 			repos_removed+=1 
-	# 		print('GithubUtilities:: Finished removing ' + str(repos_removed) + ' repo (' + repo_name + ') from team ' + team_name)
-	# 	num_repos_removed+=repos_removed
-	# return num_repos_removed
 
 def add_team_users_to_ignore_list(ignored_team_names):
 	for ignored_team_name in ignored_team_names: 
