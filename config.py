5e314ed2f880dccc1e2809aa4f3e65d83a84e48a
Andrew Stec <andrewstec@gmail.com>
2020-01-27 07:21:12 -0800

    Try-catched input config data

--- config.py
+++ config.py
@@ -3,6 +3,17 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
 
+API_TOKEN = 'api_token'
+API_PATH = 'api_path'
+GITHUB_ORG = 'github_org'
+IGNORED_TEAM_NAMES = 'ignored_team_names'
+
+API_TOKEN_PROMPT = 'Enter an API key with access to the organization and owner permissions: '
+API_PATH_PROMPT = 'What is the API path (ie. https://github-dev.students.cs.ubc.ca/api/v3)? '
+COURSE_PROMPT = 'What is your course number (ie. 436v)? '
+GITHUB_ORG_PROMPT = 'What Github organization would you like to work on? '
+IGNORED_TEAM_NAMES_PROMPT = 'What teams would you like to ignore? Please enter comma seperated teams (ie. staff, admin, students, etc.) '
+
 def get_input(text_prompt):
 	return input(text_prompt)
 
@@ -11,16 +22,13 @@ def set_value(value, prompt):
         return config[course][value]
     return get_input(prompt).strip()
 
-def get_config_value(key):
-	try:
-		if 'course' in config['DEFAULT']:
-			return config['DEFAULT'][key]
-	except KeyError:
-		print('Course {x} configuration was not found. Aborting... '.format(course))
-		exit()
+try: 
+    course = get_input(COURSE_PROMPT)
+    github_org = set_value(GITHUB_ORG, GITHUB_ORG_PROMPT)
+    api_token = set_value(API_TOKEN, API_TOKEN_PROMPT)
+    api_path = set_value(API_PATH, API_PATH_PROMPT)
+    ignored_team_names = set_value(IGNORED_TEAM_NAMES, IGNORED_TEAM_NAMES_PROMPT)
+except KeyError: 
+    print('Config.py:: Configuration error. Exiting...')
+    exit()
 
-course = get_input('What is your course number (ie. 436v)? ')
-github_org = set_value('github_org', 'What Github organization would you like to work on? ')
-api_token = set_value('api_token', 'Enter an API key with access to the organization and owner permissions: ')
-api_path = set_value('api_path', 'What is the API path (ie. https://github-dev.students.cs.ubc.ca/api/v3)? ')
-ignored_team_names = set_value('ignored_team_names', 'What teams would you like to ignore? Please enter comma seperated teams (ie. staff, admin, students, etc.) ')
