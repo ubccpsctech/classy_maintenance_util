82cc904a56886b574992fef18a583ede8bf6379d
Andrew Stec <andrewstec@gmail.com>
2020-01-27 11:15:38 -0800

    Separated procedure logic and team specific reusable business logic

--- api/github_enterprise.py
+++ api/github_enterprise.py
@@ -3,7 +3,7 @@ from network.request import request
 
 MAX_PAGE_SIZE = 30
 
-def get_org_teams(accData=[], page=1):
+def get_all_org_teams(accData=[], page=1):
 	endpoint_url = '{0}/orgs/{1}/teams?page={2}'.format(config.api_path, config.github_org, page)
 	teams = request(endpoint_url).json()
 	accData += teams
@@ -14,9 +14,9 @@ def get_org_teams(accData=[], page=1):
 
 	print('GithubUtilities:: get_org_teams() - ' + str(len(accData)) + ' teams found in ' + config.github_org)
 
-	return get_org_teams(accData, page+1)
+	return get_all_org_teams(accData, page+1)
 
-def get_team_repos(team_name, team_id, acc_data=[], page=1):	
+def get_all_repos_on_team(team_name, team_id, acc_data=[], page=1):	
 	endpoint_url = '{0}/teams/{1}/repos?page={2}'.format(config.api_path, team_id, page)
 	team_repos_page = request(endpoint_url).json()
 	acc_data += team_repos_page
@@ -26,4 +26,5 @@ def get_team_repos(team_name, team_id, acc_data=[], page=1):
 	if len(team_repos_page) < MAX_PAGE_SIZE: 
 		print('GithubUtilities:: get_team_repos() - ' + str(len(acc_data)) + ' repos found in ' + team_name)
 		return acc_data
-	return get_team_repos(team_name, team_id, acc_data, page+1)
+	return get_all_repos_on_team(team_name, team_id, acc_data, page+1)
+
